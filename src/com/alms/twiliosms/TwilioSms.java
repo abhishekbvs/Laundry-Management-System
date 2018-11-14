cf49769a9e1222912c5217c8b512636e11680dfb
abhishekbvs <bvsabhishek@gmail.com>
2018-11-14 21:19:52 +0530

    Updated the Code

--- src/com/alms/twiliosms/TwilioSms.java
+++ src/com/alms/twiliosms/TwilioSms.java
@@ -5,8 +5,8 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class TwilioSms {
-	public static final String ACCOUNT_SID = "ACbc15a09513a050bc85cc1bc88c311974";
-	public static final String AUTH_TOKEN = "86619a9fdc4c325ff7b237a22e2f724a";
+	public static final String ACCOUNT_SID = "";
+	public static final String AUTH_TOKEN = "";
 	public static void sendSMS(String ph, String body) {
 	    Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 	    Message message = Message.creator(new PhoneNumber("+91"+ph), new PhoneNumber("+12537859095"), body).create();
